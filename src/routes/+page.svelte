12ecae1acd6a46094fcdd9d3b9f0732a8fa7e218
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-29 08:43:37 -0700

    added sveltekit and supabase npms; rendered blog to sveltekit from supabase, blog was from directus

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<script>
+  export let data;
+  // let { travel } = data;
+</script>
+
+<ul>
+  {#each data.travel as {title,author,date,tags,summary,body}}
+    <p>{title}</p>
+    <p>{author}</p>
+    <p>{date}</p>
+    <p>{tags}</p>
+    <p>{summary}</p>
+    <p>{body}</p>
+  {/each}
+</ul>
